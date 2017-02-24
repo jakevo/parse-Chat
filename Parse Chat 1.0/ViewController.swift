cfc666a5432c7e557f37c7d40ebc26fd71fd5004
Jake Vo <javo@ucsd.edu>
2017-02-23 21:15:43 -0800

    Initial Commit

---
+++ Parse Chat 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parse Chat 1.0
+//
+//  Created by Jake Vo on 2/23/17.
+//  Copyright © 2017 Jake Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
